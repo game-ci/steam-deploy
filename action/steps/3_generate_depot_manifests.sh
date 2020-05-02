e614a59cb67ab4760b67c1530907b405e2e1c7d3
Webber <webber@takken.io>
2020-05-02 21:48:33 +0200

    create a monster

---
+++ action/steps/3_generate_depot_manifests.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo ""
+echo "#################################"
+echo "#   Generating Depot Manifests  #"
+echo "#################################"
+echo ""
+
+mkdir BuildOutput
+mkdir ContentRoot
+
+export DEPOTS="\n"
+i=0;
+until [ $i -gt 9 ]; do
+  eval "currentDepotPath=\$depot${i}Path"
+  if [ -z "${currentDepotPath}" ]; then
+    ((currentDepot=appId+i))
+    echo "adding depot$currentDepot.vdf"
+    export DEPOTS="$DEPOTS		\"$currentDepot\" \"depot$currentDepot.vdf\"\n"
+    cat << EOF > "depot$currentDepot.vdf"
+"DepotBuildConfig"
+{
+	"DepotID" "$currentDepot"
+	"ContentRoot"	"$(pwd)"
+  "FileMapping"
+  {
+    "LocalPath" "$rootPath/$currentDepotPath"
+    "DepotPath" "."
+    "recursive" "1"
+  }
+  "FileExclusion" "*.pdb"
+}
+EOF
+  fi;
+
+  ((i=i+1))
+done
---
+++ action/steps/4_generate_app_manifest.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo ""
+echo "#################################"
+echo "#    Generating App Manifest    #"
+echo "#################################"
+echo ""
+
+cat << EOF > "manifest.vdf"
+"appbuild"
+{
+	"appid"	"$appId"
+	"desc" "$buildDescription"
+	"buildoutput" "BuildOutput"
+	"contentroot" "ContentRoot"
+	"setlive"	"$releaseBranch"
+	"preview" "$previewEnabled"
+	"local"	"$localContentServer"
+
+	"depots"
+	{$DEPOTS}
+}
+EOF
