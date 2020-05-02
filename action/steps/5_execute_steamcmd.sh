2f4e88497b0116e6e37679b08c0cb3afdadf1510
Webber <webber@takken.io>
2020-05-03 00:11:04 +0200

    Add verbosity to output for failures 🥋

--- action/steps/5_execute_steamcmd.sh
+++ action/steps/5_execute_steamcmd.sh
@@ -1,17 +1,53 @@
 #!/bin/sh
 
+# Debugging urls:
+# https://partner.steamgames.com/doc/sdk/uploading#Troubleshooting_SteamPipe
+# https://partner.steamgames.com/doc/sdk/uploading#Debugging_Build_Issues
+#
 echo ""
 echo "#################################"
 echo "#        Uploading build        #"
 echo "#################################"
 echo ""
-
 "$STEAMCMDDIR/steamcmd.sh" \
   +login "$INPUT_USERNAME" "$INPUT_PASSWORD" \
   +run_app_build_http \
   +api_logging verbose \
   +log_ipc verbose \
-  +quit ||
-  cat /github/home/Steam/logs/stderr.txt &&
-  ls -Ralph ContentRoot &&
-  ls -Ralph BuildOutput
+  +quit || (
+    echo ""
+    echo "#################################"
+    echo "#        Current status         #"
+    echo "#################################"
+    echo ""
+    echo "Show the current state of the app on this client."
+    "$STEAMCMDDIR/steamcmd.sh app_status $appId"
+    echo ""
+    echo "Show the current Steamworks configuration for this game (depots, launch options, etc.). $appId"
+    "$STEAMCMDDIR/steamcmd.sh app_info_print $appId"
+    echo ""
+    echo "Show the current user configuration for this game (current language, install directory, etc.)"
+    "$STEAMCMDDIR/steamcmd.sh app_config_print $appId"
+    echo ""
+    echo "#################################"
+    echo "#             Errors            #"
+    echo "#################################"
+    echo ""
+    echo "home = $HOME"
+    echo ""
+    echo "Listing logs folder:"
+    echo ""
+    ls -Ralph /github/home/Steam/logs/
+    echo ""
+    echo "Listing error log"
+    echo ""
+    cat /github/home/Steam/logs/stderr.txt
+    echo ""
+    echo "#################################"
+    echo "#             Output            #"
+    echo "#################################"
+    echo ""
+    ls -Ralph ContentRoot
+    ls -Ralph BuildOutput
+    exit 1
+  )
