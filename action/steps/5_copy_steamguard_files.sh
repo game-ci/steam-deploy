40b487c92e7d90ff43b9b160177ceacd2a3baf95
David Finol <davidmfinol@gmail.com>
2021-11-26 17:22:08 -0600

    Test output

--- action/steps/5_copy_steamguard_files.sh
+++ action/steps/5_copy_steamguard_files.sh
@@ -10,7 +10,7 @@ mkdir -p /home/runner/Steam/config
 echo "$INPUT_CONFIGVDF" | base64 -d - > /home/runner/Steam/config/config.vdf
 echo "$INPUT_SSFNFILECONTENTS" | base64 -d - > "$INPUT_SSFNFILEPATH"
 
-cat /home/runner/Steam/config
+cat /home/runner/Steam/config/config.vdf
 ls /home/runner/Steam
 cat "$INPUT_SSFNFILEPATH"
 
