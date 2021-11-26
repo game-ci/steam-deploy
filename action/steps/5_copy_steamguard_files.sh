4082e344729c484ee739dbe907bb2f190250bd1d
David Finol <davidmfinol@gmail.com>
2021-11-25 22:34:37 -0600

    Improve authentication

--- action/steps/5_copy_steamguard_files.sh
+++ action/steps/5_copy_steamguard_files.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-if [ -n "$INPUT_STEAM_CONFIG_VDF" && -n "$INPUT_STEAM_SSFN_FILE_NAME" && -n "$INPUT_STEAM_SSFN_FILE_CONTENTS" ]; then
-  echo ""
-  echo "#################################"
-  echo "#    Copying SteamGuard Files    #"
-  echo "#################################"
-  echo ""
-  
+echo ""
+echo "#################################"
+echo "#    Copying SteamGuard Files    #"
+echo "#################################"
+echo ""
+
+if [ -n "$INPUT_STEAMCONFIGVDF" && -n "$INPUT_STEAMSSFNFILENAME" && -n "$INPUT_STEAMSSFNFILECONTENTS" ]; then
   mkdir -p /home/runner/Steam/config
-  echo "$INPUT_STEAM_CONFIG_VDF" | base64 -d - > /home/runner/Steam/config/config.vdf
-  echo "$INPUT_STEAM_SSFN_FILE_CONTENTS" | base64 -d - > "/home/runner/Steam/$INPUT_STEAM_SSFN_FILE_NAME"
-  
+  echo "$INPUT_STEAMCONFIGVDF" | base64 -d - > /home/runner/Steam/config/config.vdf
+  echo "$INPUT_STEAMSSFNFILECONTENTS" | base64 -d - > "/home/runner/Steam/$INPUT_STEAMSSFNFILENAME"
   echo "Copied SteamGuard Files!"
   echo ""
+else
+  echo "Not Copying SteamGuard Files!"
 fi;
