9100b8332b8a05eca3eaab5d8d008359cb97d8ad
David Finol <davidmfinol@gmail.com>
2021-11-26 14:11:56 -0600

    Simplify steamguard files copy

--- action/steps/5_copy_steamguard_files.sh
+++ action/steps/5_copy_steamguard_files.sh
@@ -6,13 +6,9 @@ echo "#    Copying SteamGuard Files    #"
 echo "#################################"
 echo ""
 
-if [ -n "$INPUT_CONFIGVDF" && -n "$INPUT_SSFNFILEPATH" && -n "$INPUT_SSFNFILECONTENTS" ]; then
-  mkdir -p /home/runner/Steam/config
-  echo "$INPUT_CONFIGVDF" | base64 -d - > /home/runner/Steam/config/config.vdf
-  echo "$INPUT_SSFNFILECONTENTS" | base64 -d - > "$INPUT_SSFNFILEPATH"
-  echo "Copied SteamGuard Files!"
-else
-  echo "Not Copying SteamGuard Files!"
-fi;
+mkdir -p /home/runner/Steam/config
+echo "$INPUT_CONFIGVDF" | base64 -d - > /home/runner/Steam/config/config.vdf
+echo "$INPUT_SSFNFILECONTENTS" | base64 -d - > "$INPUT_SSFNFILEPATH"
 
+echo "Copied SteamGuard Files!"
 echo ""
