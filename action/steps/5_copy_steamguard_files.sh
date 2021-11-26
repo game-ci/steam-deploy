3926ef0e7b552f16bc17611b35a3b3525cea2488
David Finol <davidmfinol@gmail.com>
2021-11-26 17:34:29 -0600

    Add permissions

--- action/steps/5_copy_steamguard_files.sh
+++ action/steps/5_copy_steamguard_files.sh
@@ -7,12 +7,18 @@ echo "#################################"
 echo ""
 
 mkdir -p /home/runner/Steam/config
-echo "$INPUT_CONFIGVDF" | base64 -d - > /home/runner/Steam/config/config.vdf
+echo "$INPUT_CONFIGVDF" > /home/runner/Steam/config/config.vdf
 echo "$INPUT_SSFNFILECONTENTS" | base64 -d - > "$INPUT_SSFNFILEPATH"
 
+chmod 777 /home/runner/Steam/config/config.vdf
+chmod 777 "$INPUT_SSFNFILEPATH"
+
 cat /home/runner/Steam/config/config.vdf
-ls /home/runner/Steam
+echo ""
+ls -al /home/runner/Steam
+echo ""
 cat "$INPUT_SSFNFILEPATH"
+echo ""
 
 echo "Copied SteamGuard Files!"
 echo ""
