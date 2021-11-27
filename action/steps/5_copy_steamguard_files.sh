97a09eace3c91f68f7ff11bb873fd17f3dbdd2ba
David Finol <davidmfinol@gmail.com>
2021-11-27 03:06:32 -0600

    Change path

--- action/steps/5_copy_steamguard_files.sh
+++ action/steps/5_copy_steamguard_files.sh
@@ -10,7 +10,7 @@ mkdir -p /github/home/Steam/config
 echo "$INPUT_CONFIGVDF" > /github/home/Steam/config/config.vdf
 echo "$INPUT_SSFNFILECONTENTS" | base64 -d - > "/github/home/Steam/$INPUT_SSFNFILENAME"
 
-chmod 777 /home/runner/Steam/config/config.vdf
+chmod 777 /github/home/Steam/config/config.vdf
 chmod 777 "/github/home/Steam/$INPUT_SSFNFILENAME"
 
 cat /github/home/Steam/config/config.vdf
