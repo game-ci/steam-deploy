201a608e35b1671e6ebba7863f18d011f5388d37
David Finol <davidmfinol@gmail.com>
2021-11-29 03:04:19 -0600

    Use steam executable

--- action/execute_steamcmd.sh
+++ action/execute_steamcmd.sh
@@ -9,11 +9,13 @@ echo ""
 mkdir -p /home/runner/Steam/config
 
 if [ -n "$configVdf" ]; then
+  echo "Copying /home/runner/Steam/config/config.vdf..."
   echo "$configVdf" > /home/runner/Steam/config/config.vdf
   chmod 777 /home/runner/Steam/config/config.vdf
 fi;
 
 if [ -n "$ssfnFileName" ]; then
+  echo "Copying /home/runner/Steam/ssfn..."
   echo "$ssfnFileContents" | base64 -d - > "/home/runner/Steam/$ssfnFileName"
   chmod 777 "/home/runner/Steam/$ssfnFileName"
 fi;
@@ -21,61 +23,12 @@ fi;
 echo "Finished Copying SteamGuard Files!"
 echo ""
 
-# Debugging urls:
-# https://partner.steamgames.com/doc/sdk/uploading#Troubleshooting_SteamPipe
-# https://partner.steamgames.com/doc/sdk/uploading#Debugging_Build_Issues
-#
 echo ""
 echo "#################################"
 echo "#        Uploading build        #"
 echo "#################################"
 echo ""
-"$STEAMCMDDIR/steamcmd.sh" \
-  +login "$username" "$password" "$mfaCode" \
-  +run_app_build $(pwd)/manifest.vdf \
-  +quit || (
-    echo "#################################"
-    echo "#        Current status         #"
-    echo "#################################"
-    echo ""
-    echo "Show the current state of the app on this client."
-    sleep 1.0e-2
-    "$STEAMCMDDIR/steamcmd.sh +app_status $appId"
-    echo ""
-    echo "Show the current Steamworks configuration for this game (depots, launch options, etc.). $appId"
-    sleep 1.0e-2
-    "$STEAMCMDDIR/steamcmd.sh +app_info_print manifest.vdf"
-    echo ""
-    echo "Show the current user configuration for this game (current language, install directory, etc.)"
-    sleep 1.0e-2
-    "$STEAMCMDDIR/steamcmd.sh +app_config_print $appId"
-    echo ""
-    echo "#################################"
-    echo "#             Errors            #"
-    echo "#################################"
-    echo ""
-    echo "Listing current folder and rootpath"
-    echo ""
-    ls -alh
-    echo ""
-    ls -alh $rootPath
-    echo ""
-    echo "Listing logs folder:"
-    echo ""
-    ls -Ralph /home/runner/Steam/logs/
-    echo ""
-    echo "Displaying error log"
-    echo ""
-    cat /home/runner/Steam/logs/stderr.txt
-    echo ""
-    echo "Displaying bootstrapper log"
-    echo ""
-    cat /home/runner/Steam/logs/bootstrap_log.txt
-    echo ""
-    echo "#################################"
-    echo "#             Output            #"
-    echo "#################################"
-    echo ""
-    ls -Ralph BuildOutput
-    exit 1
-  )
+
+echo "$steamExecutable"
+"$steamExecutable" +login "$username" "$password" "$mfaCode" +run_app_build $(pwd)/manifest.vdf +quit
+echo ""
