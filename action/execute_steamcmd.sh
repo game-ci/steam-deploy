de169bd27f6af5e5b330cd86886b00cdcfcbe23d
David Finol <davidmfinol@gmail.com>
2021-11-28 18:53:07 -0600

    Simplify action

--- action/execute_steamcmd.sh
+++ action/execute_steamcmd.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+echo ""
+echo "#################################"
+echo "#    Copying SteamGuard Files   #"
+echo "#################################"
+echo ""
+
+mkdir -p /home/runner/Steam/config
+echo "$configVdf" > /home/runner/Steam/config/config.vdf
+echo "$ssfnFileContents" | base64 -d - > "/home/runner/Steam/$ssfnFileName"
+
+chmod 777 /home/runner/Steam/config/config.vdf
+chmod 777 "/home/runner/Steam/$ssfnFileName"
+
+echo "Copied SteamGuard Files!"
+echo ""
+
 # Debugging urls:
 # https://partner.steamgames.com/doc/sdk/uploading#Troubleshooting_SteamPipe
 # https://partner.steamgames.com/doc/sdk/uploading#Debugging_Build_Issues
--- action/generate_manifests.sh
+++ action/generate_manifests.sh
@@ -37,3 +37,29 @@ EOF
 
   i=$((i+1))
 done
+
+echo ""
+echo "#################################"
+echo "#    Generating App Manifest    #"
+echo "#################################"
+echo ""
+
+mkdir BuildOutput
+
+cat << EOF > "manifest.vdf"
+"appbuild"
+{
+  "appid" "$appId"
+  "desc" "$buildDescription"
+  "buildoutput" "BuildOutput"
+  "contentroot" "$(pwd)"
+  "setlive" "$releaseBranch"
+
+  "depots"
+  {$(echo "$DEPOTS" | sed 's/\\n/\
+/g')}
+}
+EOF
+
+cat manifest.vdf
+echo ""
